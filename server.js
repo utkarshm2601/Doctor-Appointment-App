aec972e9a25fc388ab344c7fc279561b4050756a
UTKARSH MISHRA <saumyachs12@gmail.com>
2024-07-01 00:00:18 +0530

    Created Server along with connection with the database.Created routing fuctionality along with login and register functionality.Authorization using JWT token and created user model.

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const colors = require("colors");
+const moragan = require("morgan");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+//dotenv conig
+dotenv.config();
+
+//mongodb connection
+connectDB();
+
+//rest obejct
+const app = express(); //express ke saare features is object me aa jaayenge
+
+//middlewares
+app.use(express.json()); // cors ya parse ka issue na aaye.Using this we will not get any error related to parsing wrna jb bhi agr hum body ke andar req me json object bhejenge toh usme hume error dekhne milega.Iske alawa we can use body parser package
+app.use(moragan("dev"));
+
+//routes
+app.use("/api/v1/user", require("./routes/userRoutes"));
+
+//port
+const port = process.env.PORT || 8080;
+//listen port
+app.listen(port, () => {
+  console.log(
+    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+      .bgCyan.white
+  );
+});
