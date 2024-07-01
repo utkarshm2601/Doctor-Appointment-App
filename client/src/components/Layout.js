aa6247da93f8423c189c2792f897185a9770a6ca
shubh062 <shubhamraj2016aur@gmail.com>
2024-07-01 14:45:59 +0530

    Complete Layout and sidebar.

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "../styles/LayoutStyles.css";
-import { SidebarMenu } from "./../Data/data";
+import { adminMenu, userMenu } from "./../Data/data";
 
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { message } from "antd";
 const Layout = ({ children }) => {
+  const { user } = useSelector((state) => state.user);
   const location = useLocation();
+  const navigate = useNavigate();
+  // logout funtion
+  const handleLogout = () => {
+    localStorage.clear();
+    message.success("Logout Successfully");
+    navigate("/login");
+  };
+
+  // redering menu list
+  const SidebarMenu = user?.isAdmin ? adminMenu : userMenu;
   return (
     <>
       <div className="main">
@@ -26,10 +39,19 @@ const Layout = ({ children }) => {
                   </>
                 );
               })}
+              <div className={`menu-item `} onClick={handleLogout}>
+                <i className="fa-solid fa-right-from-bracket"></i>
+                <Link to="/login">Logout</Link>
+              </div>
             </div>
           </div>
           <div className="content">
-            <div className="header">Header</div>
+            <div className="header">
+              <div className="header-content">
+                <i class="fa-solid fa-bell"></i>
+                <Link to="/profile">{user?.name}</Link>
+              </div>
+            </div>
             <div className="body">{children}</div>
           </div>
         </div>
--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -54,7 +54,8 @@ const loginController = async (req, res) => {
 
 const authController = async (req, res) => {
   try {
-    const user = await userModel.findOne({ _id: req.body.userId });
+    const user = await userModel.findById({ _id: req.body.userId });
+    user.password = undefined;
     if (!user) {
       return res.status(200).send({
         message: "user not found",
@@ -63,10 +64,7 @@ const authController = async (req, res) => {
     } else {
       res.status(200).send({
         success: true,
-        data: {
-          name: user.name,
-          email: user.email,
-        },
+        data: user,
       });
     }
   } catch (error) {
